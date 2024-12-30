622abbb9ab43c081daeb65cdcfe0a18c7218c2fc
Masud <ranamasud0701@gmail.com>
2024-12-30 08:54:30 +0600

    added the more javascript

---
+++ index.js
@@ -0,0 +1,4 @@
+function changeFunction(){
+    document.getElementById('change').innerHTML="new Bangladesh"+" "+"all the clear bad behavier"
+    document.getElementById('change').style.fontSize='60px'
+}
---
+++ keyword.js
@@ -0,0 +1,36 @@
+
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+// console.log(z)
+
+// const x = 5;
+// const y = 6;
+// const z = x + y;
+// console.log(z)
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total)
+
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 5;
+console.log($myMoney,$$$,$)
+
+let s = 2 + 3 + "5";
+console.log(s);
+
+let c = "2" + 3 + 5;
+console.log(c);
+
+ let x = 10;
+// Here x is 10
+
+{  
+  let x = 2;
+  // Here x is 2
+}
+console.log(x)// but display show the result = 10
+
